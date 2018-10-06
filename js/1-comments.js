353560b78f27a94cad3ec8927efe6ab0eb7f776b
Adam Grabowski <13grabek@wp.pl>
2018-10-06 10:04:20 +0200

    czesc pierwsza

---
+++ js/1-comments.js
@@ -0,0 +1,2 @@
+//console.log("Działa !");
+/*console.log("Działa !");*/
---
+++ js/2-varibles.js
@@ -0,0 +1,16 @@
+console.log("varibles");
+
+let imie;
+let imieMeskie = "Kacper";
+
+imie = "Monika";
+
+imie = "Gosia";
+
+imie = imieMeskie;
+
+const liczba;
+
+liczba = 10;
+
+console.log(liczba);
